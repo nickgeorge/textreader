2e639e035cc5632dc8b932d6d05bfd5abbe79d46
Nick George <nickjpgeorge@gmail.com>
2014-02-05 00:18:22 -0800

    tweak fuzzy matcher

--- client/common/fuzzy_scorer.js
+++ client/common/fuzzy_scorer.js
@@ -3,7 +3,6 @@
  */
 FuzzyScorer = function(options) {
   this.options = options;
-  // this.options = [options[2]];
 };
 
 
@@ -25,32 +24,8 @@ FuzzyScorer.option;
  */
 FuzzyScorer.match;
 
-
-FuzzyScorer.prototype.match = function(searchString, opt_caseSensitive) {
-  if (!opt_caseSensitive) {
-    searchString = searchString.toLowerCase();
-  }
-  var matches = this.options.map(function(option) {
-    var response = this.scoreOption(searchString, option.text.toLowerCase());
-    return {
-      option: option,
-      score: response.score,
-      indices: response.indices
-    };
-  }, this);
-
-  matches = matches.filter(function(match) {
-    return match.score > 0;
-  });
-  matches.sort(function(a, b) {
-    return b.score - a.score;
-  });
-  return matches;
-};
-
 /**
  * @typedef {
- *   score: number,
  *   searchIndex: number,
  *   optionIndex: number,
  *   consecutive: number
@@ -64,21 +39,28 @@ FuzzyScorer.INITIAL_STATE_ = {
   consecutive: 0
 };
 
-/**
- * @typedef {
- *   score: number,
- *   indices: Array.<number>
- * }
- */
-FuzzyScorer.Response_;
-
 FuzzyScorer.INVALID_ = -1;
 
-FuzzyScorer.prototype.scoreOption = function(searchString, option, opt_state) {
-  var state = opt_state || FuzzyScorer.INITIAL_STATE_;
 
+FuzzyScorer.prototype.match = function(searchString) {
+  var matches = this.options.map(function(option) {
+    return this.scoreOption(searchString.toLowerCase(), option,
+        FuzzyScorer.INITIAL_STATE_);
+  }, this);
+
+  matches = matches.filter(function(match) {
+    return match.score > 0;
+  });
+  matches.sort(function(a, b) {
+    return b.score - a.score;
+  });
+  return matches;
+};
+
+FuzzyScorer.prototype.scoreOption = function(searchString, option, state) {
   if (state.searchIndex == searchString.length) {
     return {
+      option: option,
       score: 0,
       indices: new Array(searchString.length)
     };
@@ -86,23 +68,22 @@ FuzzyScorer.prototype.scoreOption = function(searchString, option, opt_state) {
 
   var letterToMatch = searchString[state.searchIndex];
   var bestResponse = {
-    score: FuzzyScorer.INVALID_,
-    indices: []
+    score: FuzzyScorer.INVALID_
   };
-  for (var i = state.optionIndex; i < option.length; i++) {
-    if (option[i] == letterToMatch) {
-      var newState = util.object.shallowClone(state);
+  for (var i = state.optionIndex; i < option.text.length; i++) {
+    if (option.text[i].toLowerCase() == letterToMatch) {
+      var consecutive = 0;
       if (i > 0 && i == state.optionIndex) {
-        newState.consecutive++;
-      } else {
-        newState.consecutive = 0;
+        consecutive = state.consecutive + 1;
       }
-      var scoreFromThisStep = Math.pow(2, newState.consecutive)
-      newState.searchIndex++;
-      newState.optionIndex = i + 1;
+      var newState = {
+        searchIndex: state.searchIndex + 1,
+        optionIndex: i + 1,
+        consecutive: consecutive
+      };
       var response = this.scoreOption(searchString, option, newState);
       if (response.score == FuzzyScorer.INVALID_) continue;
-      response.score += scoreFromThisStep;
+      response.score += Math.pow(2, consecutive);
       response.indices[i] = true;
       if (response.score  > bestResponse.score) {
         bestResponse = response;
